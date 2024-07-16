dd23ebbf566384b9809dcf3d6f0b7782f91f857d
Krinal Sojitra <krinalSojitra21@gmail.com>
2024-07-16 17:23:47 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen  w-full flex-col items-center gap-10 ">
+      Hello
+    </main>
+  );
+}
