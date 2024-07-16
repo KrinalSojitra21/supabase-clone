5cc7428225d51ba3dc0ead91aed1bbd9c0ffad04
Krinal Sojitra <krinalSojitra21@gmail.com>
2024-07-16 17:26:40 +0530

    Add: CommunitySection, ExamplesSection, InstantAPIsSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,23 @@
 import React from "react";
 
 import "./globals.css";
+import TitleBar from "@/components/shared/TitleBar";
+
+import CommunitySection from "./CommunitySection";
+import HeroSection from "./HeroSection";
+import ExamplesSection from "./ExamplesSection";
+import InstantAPIsSection from "./InstantAPIsSection";
+import FeatureSection from "./FeatureSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen  w-full flex-col items-center gap-10 ">
-      Hello
+      <TitleBar />
+      <HeroSection />
+      <FeatureSection />
+      <CommunitySection />
+      <ExamplesSection />
+      <InstantAPIsSection />
     </main>
   );
 }
